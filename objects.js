b81ac9cff50a2a8b3ad6767f58a5880252d84341
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-01 03:05:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,1 @@
-var playlist = {name1:'Title1', name2:'Title2', name3='Title3'}
-
-function updatePlaylist(object, name, title)
-{
-  object[name]= title;
-  return object
-}
-
-function removeFromPlaylist(object, name)
-{
-  delete object[name]
-  return object
-}
+var playlist = new Object() {name1:'Title1', name2:'Title2', name3='Title3'}
